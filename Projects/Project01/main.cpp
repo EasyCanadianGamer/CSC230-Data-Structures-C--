f1055d92e69e6b2d523d0668ca7cd23713ab1dc9
EasyCanadianGamer <easygamer2019@gmail.com>
2024-02-22 21:18:36 -0500

    modified:   Projects/Project01/a.exe 	modified:   Projects/Project01/main.cpp

--- Projects/Project01/main.cpp
+++ Projects/Project01/main.cpp
@@ -4,11 +4,9 @@
 
 using namespace std;
 
-bool checkL(vector<vector<char>> &matrix, char *word, int x);
-bool checkR(vector<vector<char>> &matrix, char *word, int x);
-bool checkU(vector<vector<char>> &matrix, char *word, int x);
-bool checkD(vector<vector<char>> &matrix, char *word, int x);
-bool checkW(vector<vector<char>> &matrix, char *word);
+bool search(vector<vector<char>>& matrix, char* word, int x, int y, int dx, int dy);
+
+
 int len(char *word);
 
 
@@ -24,7 +22,7 @@ int main(int argc, char *argv[]) {
 
 
     vector<vector<char>> vect(x, vector<char>(y)); 
-    vector<vector<char>> vect1(x, vector<char>(y)); 
+    //vector<vector<char>> vect1(x, vector<char>(y)); 
 
 
    
@@ -42,147 +40,37 @@ int main(int argc, char *argv[]) {
         cout << endl;
     }
 
-    int wordLength = len(argv[1]);
-
-
-    if (checkW(vect, argv[1])) {
-        for ( int i = 1; i < wordLength+1; i++)
+    int s = argc - 1; 
+   for( int k = 1; k < s ; k++)
+   {
+    for( int i =0; i < x; i++)
+    {
+        for ( int j = 0; j < y; j++)
         {
 
-            for ( int k = i; k < i; k++)
-            {
-                if (checkR(vect,argv[1],k))
-                {  
-                    cout << "Word not found!" << endl;
-                }
-                if ( checkL(vect,argv[1],k))
-                {
-                    cout << "Word not found!" << endl;
-                }
-                if ( checkD(vect,argv[1],k))
-                {
-                    cout << "Word not found!" << endl;
-                }
-                if ( checkU(vect,argv[1],k))
-                {
-                    cout << "Word not found!" << endl;
-                }
-                else
-                {
-                    cout << "Word not found!" << endl;
-                    break;
-                }
-            }
-            }
-
-                    cout << "letter found!" << endl;
         }
-        else
-        {
-            cout << "letter Not found!" << endl;
-        }
-   
-    
-
-        
+    }
+   }
 
     return 0;
 } 
-// this checks the first letter
-bool checkW(vector<vector<char>> &matrix, char *word) {
-    int rows = matrix.size();
-    int cols = matrix[0].size();
 
-    for( int i = 0; i < rows; i++)
-    {
-        for (int j= 0; j< cols; j++){
-            bool match = true;
-            if (matrix[i][j] != word[0]) {
-                match = false;
-            }
-        
-        if (match) return true;    
-        }
-    }
-
-    return false;
-}
-
-int len( char *word)
+bool search(vector<vector<char>>& matrix, char* word, int x, int y, int dx, int dy) 
 {
-    int wordLength = strlen(word);   
+        int wordLength = strlen(word);   
+        int x = x + dx;
+        int y = x + dy;
 
-    return wordLength;
-}
-bool checkL(vector<vector<char>>& matrix, char* word, int x) {
-    int wordLength = strlen(word);
-    int cols = matrix[0].size();
+        for( int i = 0; i < wordLength; i++)
+        {
 
-    for (int j = 0; j < cols; ++j) {
-        bool match = true;
-        for (int k = 0; k < wordLength; ++k) {
-            if (j + k >= cols || matrix[x][j + k] != word[k]) {
-                match = false;
-                break;
+            bool match = false;
+            if(word[i] == matrix [x][y])
+            {
+                match = true;
             }
+            if(match) return false;
         }
-        if (match) return true;
-    }
+        return true;
 
-    return false;
 }
-
-bool checkR(vector<vector<char>>& matrix, char* word, int x) {
-    int wordLength = strlen(word);
-    int cols = matrix[0].size();
-
-    for (int j = cols - 1; j >= 0; --j) {
-        bool match = true;
-        for (int k = 0; k < wordLength; ++k) {
-            if (j - k < 0 || matrix[x][j - k] != word[k]) {
-                match = false;
-                break;
-            }
-        }
-        if (match) return true;
-    }
-
-    return false;
-}
-
-bool checkU(vector<vector<char>>& matrix, char* word, int x) {
-    int wordLength = strlen(word);
-    int rows = matrix.size();
-
-    for (int i = 0; i < rows; ++i) {
-        bool match = true;
-        for (int k = 0; k < wordLength; ++k) {
-            if (i + k >= rows || matrix[i + k][x] != word[k]) {
-                match = false;
-                break;
-            }
-        }
-        if (match) return true;
-    }
-
-    return false;
-}
-
-bool checkD(vector<vector<char>>& matrix, char* word, int x) {
-    int wordLength = strlen(word);
-    int rows = matrix.size();
-
-    for (int i = rows - 1; i >= 0; --i) {
-        bool match = true;
-        for (int k = 0; k < wordLength; ++k) {
-            if (i - k < 0 || matrix[i - k][x] != word[k]) {
-                match = false;
-                break;
-            }
-        }
-        if (match) return true;
-    }
-
-    return false;
-}
-
