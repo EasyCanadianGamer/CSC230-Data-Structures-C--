9212d89c8e8401e8bbdfbe8528ebf2dc12a66753
CanadianGamer <easygamer2019@gmail.com>
2024-02-23 21:13:48 -0500

    modified:   .vscode/settings.json 	modified:   Projects/Project01/a.exe 	modified:   Projects/Project01/main.cpp 	new file:   Projects/Project01/pp.cpp

--- Projects/Project01/main.cpp
+++ Projects/Project01/main.cpp
@@ -1,30 +1,44 @@
+#include "colormod.h"
 #include <iostream>
 #include <vector>
 #include <cstring> 
 
 using namespace std;
 
-bool search(vector<vector<char>>& matrix, char* word, int x, int y, int dx, int dy);
+bool search(vector<vector<char>>& matrix, char* word, int x, int y, int dx, int dy, vector<vector<int>>& matrix2);
 
+void record(vector<vector<int>>& matrix2,  int i, int j, int x);
 
-int len(char *word);
+void printMatrix(vector<vector<int>>& matrix)
+{
+    int x = matrix.size();
 
+    for (int i = 0; i < x; i++)
+    {
+        int y = matrix[i].size(); 
 
+        for (int j = 0; j < y; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 
 int main(int argc, char *argv[]) {
     int x, y;
     char c;
-    char str[100];
 
     cin >> x;
     cin >> y;
 
 
     vector<vector<char>> vect(x, vector<char>(y)); 
-    //vector<vector<char>> vect1(x, vector<char>(y)); 
+    vector<vector<int>> vect1(x, vector<int>(y)); 
 
-
+    Color::Setting red(Color::FG_RED); 
+    Color::Setting def(Color::FG_DEFAULT); 
    
     for (int i = 0; i < x; i++) {
         for (int j = 0; j < y; j++) {
@@ -33,44 +47,80 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < y; j++) {
-            cout << vect[i][j] << " ";
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                vect1[i][j] = 0;
         }
-        cout << endl;
     }
 
-    int s = argc - 1; 
-   for( int k = 1; k < s ; k++)
+   for( int k = 0; k < argc-1 ; k++)
    {
-    for( int i =0; i < x; i++)
+        for( int i =0; i < x; i++)
     {
         for ( int j = 0; j < y; j++)
         {
+            if( search(vect,argv[1 +k],i,j, 0, 0,vect1))
+            {
 
+                if(search(vect,argv[1 +k],i,j, 0, 1,vect1) || search(vect,argv[1 +k],i,j, 0, -1,vect1) || 
+                   search(vect,argv[1 + k],i,j, 1, 0,vect1) || search(vect,argv[1 + k],i,j, -1, 0,vect1) || 
+                   search(vect,argv[1 + k],i,j, 1, 1,vect1) || search(vect,argv[1 + k],i,j, -1, -1,vect1)||
+                   search(vect,argv[1 + k],i,j, -1, 1,vect1)|| search(vect,argv[1 +k],i,j, 1, -1,vect1))
+                   {                                     
+                         cout << "This ->" << red << "Hello, world!" << def << "<- is red." << endl;
+                         
+                   }
+            }
+     
         }
+        
+
     }
+    
    }
+
+
+           
+
+
+            
+            
+
 
     return 0;
 } 
 
-bool search(vector<vector<char>>& matrix, char* word, int x, int y, int dx, int dy) 
+
+
+bool search(vector<vector<char>>& matrix, char* word, int x, int y, int dx, int dy, vector<vector<int>>& matrix2) 
 {
-        int wordLength = strlen(word);   
-        int x = x + dx;
-        int y = x + dy;
+    int wordLength = strlen(word);
 
-        for( int i = 0; i < wordLength; i++)
-        {
+    for (int i = 0; i < wordLength; i++)
+    {
+        int newX = x + dx;
+        int newY = y + dy;
 
-            bool match = false;
-            if(word[i] == matrix [x][y])
+        if (newX >= 0 && newX < matrix.size() && newY >= 0 && newY < matrix[0].size()) {
+            if (word[i] == matrix[newX][newY])
             {
-                match = true;
+                int d = 1;
+                record(matrix2, x, y, d);
+                return true;
             }
-            if(match) return false;
         }
-        return true;
+        else {
+            break; 
+        }
+    }
 
+    int d = 0;
+    record(matrix2, x, y, d);
+    return false;
+}
+
+
+void record(vector<vector<int>>& matrix2, int i, int j, int x)
+{
+    matrix2[i][j] = x;
 }
